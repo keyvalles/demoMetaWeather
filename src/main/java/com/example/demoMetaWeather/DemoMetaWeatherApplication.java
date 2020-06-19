54b748a19c0b757c6fa27849e4f78b3a21075051
keyvalles <keylis.valles@gmail.com>
2020-06-19 16:11:35 -0400

    first commit and settings

---
+++ src/main/java/com/example/demoMetaWeather/DemoMetaWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoMetaWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMetaWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMetaWeatherApplication.class, args);
+	}
+
+}
